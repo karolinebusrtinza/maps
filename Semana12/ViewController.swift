cd43c18b5fe1120e4e3eb5bcc3fec7a9463e9f59
karolinebusrtinza <bustinzakaroline@gmail.com>
2021-06-05 15:57:06 -0500

    Initial Commit

---
+++ Semana12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Semana12
+//
+//  Created by mbtec22 on 6/5/21.
+//  Copyright © 2021 Tecsup.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
